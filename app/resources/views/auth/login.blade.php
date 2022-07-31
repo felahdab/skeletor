f5bd9054e464af1a4a1e7404cbb7b7669c50a54e
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2022-07-31 23:37:55 +0200

    Started modifying the application to fit the objective. Imported logos and images. Removes useless copyright footer. Removed the possibility to register a user.

--- app/resources/views/auth/login.blade.php
+++ app/resources/views/auth/login.blade.php
@@ -1,26 +1,27 @@
-@extends('layouts.auth-master')
+@extends('layouts.app-master')
+
 
 @section('content')
+    <div>
+        <img src='{!! url("assets/images/insigneEscouade.jpg") !!}' alt="Logo de l'escouade" style="height:450px; display: block; margin-left:auto; margin-right: auto; ">
+    </div>
+    <div class="bg-light p-4 rounded" style="max-width: 400px; margin-left:auto; margin-right: auto;">
     <form method="post" action="{{ route('login.perform') }}">
         
         <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-        <img class="mb-4" src="{!! url('images/bootstrap-logo.svg') !!}" alt="" width="72" height="57">
         
-        <h1 class="h3 mb-3 fw-normal">Login</h1>
 
         @include('layouts.partials.messages')
 
-        <div class="form-group form-floating mb-3">
+        <div class="mb-3">
             <input type="text" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email" required="required" autofocus>
-            <label for="floatingName">Email or Username</label>
             @if ($errors->has('username'))
                 <span class="text-danger text-left">{{ $errors->first('username') }}</span>
             @endif
         </div>
         
-        <div class="form-group form-floating mb-3">
+        <div class="mb-3">
             <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Password" required="required">
-            <label for="floatingPassword">Password</label>
             @if ($errors->has('password'))
                 <span class="text-danger text-left">{{ $errors->first('password') }}</span>
             @endif
@@ -28,6 +29,6 @@
 
         <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
         
-        @include('auth.partials.copy')
     </form>
+    </div>
 @endsection
--- app/resources/views/home/index.blade.php
+++ app/resources/views/home/index.blade.php
@@ -1,16 +1,7 @@
 @extends('layouts.app-master')
 
 @section('content')
-    <div class="bg-light p-5 rounded">
-        @auth
-        <h1>Dashboard</h1>
-        <p class="lead">Only authenticated users can access this section.</p>
-        <a class="btn btn-lg btn-primary" href="https://codeanddeploy.com" role="button">View more tutorials here &raquo;</a>
-        @endauth
-
-        @guest
-        <h1>Homepage</h1>
-        <p class="lead">Your viewing the home page. Please login to view the restricted data.</p>
-        @endguest
+    <div>
+        <img src='{!! url("assets/images/insigneEscouade.jpg") !!}' alt="Logo de l'escouade" style="height:450px; display: block; margin-left:auto; margin-right: auto; ">
     </div>
 @endsection
