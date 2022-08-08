f02efb96bb3c50d0e9311d6c8f0ac4683b339e87
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2022-08-08 22:58:31 +0200

    Created view to change user password.

---
+++ app/resources/views/changepasswd/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app-master')
+
+@section('content')
+<div class="bg-light p-4 rounded">
+        <h1>Changement de mot de passe pour {{ $user->name }}</h1>
+	<div class="container mt-4">
+	<form method="post" action="{{ route('changepasswd.store', $user->id) }}">
+		@csrf
+		<input id="userid" name="userid" type="hidden" value="{{$user->id}}">
+		<div class="form-group form-floating mb-3">
+            <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Password" required="required">
+            <label for="floatingPassword">Password</label>
+            @if ($errors->has('password'))
+                <span class="text-danger text-left">{{ $errors->first('password') }}</span>
+            @endif
+        </div>
+
+        <div class="form-group form-floating mb-3">
+            <input type="password" class="form-control" name="password_confirmation" value="{{ old('password_confirmation') }}" placeholder="Confirm Password" required="required">
+            <label for="floatingConfirmPassword">Confirm Password</label>
+            @if ($errors->has('password_confirmation'))
+                <span class="text-danger text-left">{{ $errors->first('password_confirmation') }}</span>
+            @endif
+        </div>
+		
+		<button type="submit" class="btn btn-primary">Modifier</button>
+		<a href="{{ route('home.index') }}" class="btn btn-default">Annuler</button>
+	</form>
+</div>
+</div>
+@endsection
