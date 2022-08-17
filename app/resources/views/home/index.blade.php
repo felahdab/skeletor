451b8dd4461cf3425c6da2ba78374b2a047c227a
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2022-08-17 22:26:35 +0200

    Corrected reference to InsigneEscouade for case sensitive file systems

--- app/resources/views/home/index.blade.php
+++ app/resources/views/home/index.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div>
-        <img src='{!! url("assets/images/insigneEscouade.jpg") !!}' alt="Logo de l'escouade" style="height:450px; display: block; margin-left:auto; margin-right: auto; ">
+        <img src='{!! url("assets/images/InsigneEscouade.jpg") !!}' alt="Logo de l'escouade" style="height:450px; display: block; margin-left:auto; margin-right: auto; ">
     </div>
 @endsection
