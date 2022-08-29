88bf3684e92337fa8859dd6730144daceb4910e6
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2022-08-29 17:17:54 +0200

    Initialized StatistiqueController and setup first route.

---
+++ app/app/Http/Controllers/StatistiqueController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StatistiqueController extends Controller
+{
+    public function index() 
+    {
+        return "ici";
+    }
+}
