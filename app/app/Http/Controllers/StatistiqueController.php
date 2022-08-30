228e8d859ff054e236e3f108162551f1949d3d0b
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2022-08-30 14:53:36 +0200

    Mise au point du controller Statistiques

--- app/app/Http/Controllers/StatistiqueController.php
+++ app/app/Http/Controllers/StatistiqueController.php
@@ -6,15 +6,28 @@ use Illuminate\Support\Carbon;
 
 use Illuminate\Http\Request;
 
+use App\Models\Statistique;
+
 class StatistiqueController extends Controller
 {
     public function index(Request $request) 
     {
-        if ($request->has("month"))
-            $month = $request["month"];
+        $date_stat = Carbon::now();
+        $defaultperiod = $date_stat->year . "/" . $date_stat->month;
+        
+        if ($request->has("period"))
+            $period = $request["period"];
         else
-            $month = date("Y-m-1");
-        ddd($month);
-        return view('statistiques.index', ['month' => $month]);
+        {
+            $period = $defaultperiod;
+        }
+        
+        $statistiques = Statistique::all()->where("periode", $period);
+        
+        $liste_des_periodes = Statistique::all()->pluck('periode')->sortBy('periode')->unique();
+        
+        return view('statistiques.index', ['period' => $period,
+                                           'liste_des_periodes' => $liste_des_periodes,
+                                           'statistiques'       => $statistiques]);
     }
 }
