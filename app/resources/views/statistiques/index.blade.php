5156389108c0e33264cf4b7f5eb0b752aeec78c3
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2022-08-30 14:54:24 +0200

    Mise au point de la vue des statistiques. Désactivation pour le moment de la partie "Temps de lacher GTR" dont le besoin doit etre precise.

--- app/resources/views/statistiques/index.blade.php
+++ app/resources/views/statistiques/index.blade.php
@@ -13,7 +13,13 @@
         <div class='flex' style='justify-content: start;'>
             <nav class='navbar-brand bg-light border' style='width: 15rem; height: auto; flex-direction: column; justify-content: start ;'>
                 <div class='titrenavbarvert'>Statistiques</div>
-                <div class='mb-2 ml-2 mt-2 mr-2'><select name='listdates' id='listdates' class='custom-select w-100' onchange='modifparam("month","listdates");'><option value ='2022-07-05' >juillet 2022</option><option value ='2022-06-05' >juin 2022</option><option value ='2022-05-05' >mai 2022</option><option value ='2022-04-05' >avril 2022</option><option value ='2022-03-05' >mars 2022</option><option value ='2022-02-05' >février 2022</option><option value ='2022-01-05' >janvier 2022</option><option value ='2021-12-05' >décembre 2021</option><option value ='2021-11-05' >novembre 2021</option><option value ='2021-10-05' >octobre 2021</option><option value ='2021-09-05' >septembre 2021</option><option value ='2021-08-05' >août 2021</option></select></div>
+                <div class='mb-2 ml-2 mt-2 mr-2'>
+                    <select name='listdates' id='listdates' class='custom-select w-100' onchange='modifparam("period","listdates");'>
+                    @foreach ($liste_des_periodes as $per)
+                        <option value ='{{$per}}'>{{$per}}</option>
+                     @endforeach
+                    </select>
+                </div>
                 <form
                     method='post' action='#'>
                     <div class='mb-2 mt-2 text-center'><button class='btn btn-primary' type='submit' id='btnstat' name='btnstat'>Calculer</button></div>
@@ -22,7 +28,7 @@
                     <div class='mb-2 ml-4'><a class='navbar-brand-sm text-secondary' href='#' onclick='annuler("indic");annuler("durspe");affichage("durmarin");annuler("durgtr");'>Temps de l&acirc;cher (marin)</a></div>
                     <hr>
                     <div class='mb-2 ml-4'><a class='navbar-brand-sm text-secondary' href='#' onclick='annuler("indic");affichage("durspe");annuler("durmarin");annuler("durgtr");'>Temps de l&acirc;cher (sp&eacute;)</a></div>
-                    <div class='mb-2 ml-4'><a class='navbar-brand-sm text-secondary' href='#' onclick='annuler("indic");annuler("durspe");annuler("durmarin");affichage("durgtr");'>Temps de l&acirc;cher (gtr)</a></div>
+                    <!--<div class='mb-2 ml-4'><a class='navbar-brand-sm text-secondary' href='#' onclick='annuler("indic");annuler("durspe");annuler("durmarin");affichage("durgtr");'>Temps de l&acirc;cher (gtr)</a></div> -->
             </nav>
             <div id='indic' class='card bg-light ml-3 w-50' style=''>
                 <div class='card-header'>Indicateurs 2PS (marin d&eacute;barquant vers une FREMM) </div>
@@ -30,23 +36,23 @@
                     <table class='table table-hover'>
                         <tr>
                             <td>Nb marins</td>
-                            <td class='w-25'>0</td>
+                            <td class='w-25'>{{$statistiques->count()}}</td>
                         </tr>
                         <tr>
                             <td>Taux SAE achev&eacute;</td>
-                            <td>0%</td>
+                            <td>{{$statistiques->avg('taux_stage_valides')}}%</td>
                         </tr>
                         <tr>
                             <td>Taux compagnonnage achev&eacute;</td>
-                            <td>0%</td>
+                            <td>{{$statistiques->avg('taux_comp_valides')}}%</td>
                         </tr>
                         <tr>
                             <td>Taux transformation</td>
-                            <td>0%</td>
+                            <td>{{$statistiques->avg('taux_de_transformation')}}%</td>
                         </tr>
                         <tr>
                             <td>Nb jours au GTR</td>
-                            <td>0</td>
+                            <td>{{$statistiques->avg('nb_jour_gtr')}}</td>
                         </tr>
                     </table>
                 </div>
@@ -55,19 +61,29 @@
                     <table class='table table-hover'>
                         <tr title=''>
                             <td>Nb de marins</td>
-                            <td class='w-25'>0</td>
+                            <td class='w-25'>{{$statistiques->where('gpmt', 'OPS')->count()}}</td>
                         </tr>
                         <tr>
                             <td>Nb marins l&acirc;ch&eacute;s quai</td>
-                            <td>0</td>
+                            <td>{{ $statistiques
+                                    ->where('gpmt', 'OPS')
+                                    ->where('nb_jour_pour_lache_quai', '>', 0)
+                                    ->count() }}</td>
                         </tr>
                         <tr>
                             <td>Nb marins l&acirc;ch&eacute;s mer</td>
-                            <td>0</td>
+                            <td>{{ $statistiques
+                                       ->where('gpmt', 'OPS')
+                                       ->where('nb_jour_pour_lache_mer', '>', 0)
+                                       ->count() }}</td>
                         </tr>
                         <tr>
                             <td>Nb marins l&acirc;ch&eacute;s quai+mer</td>
-                            <td>0</td>
+                            <td>{{ $statistiques
+                                        ->where('gpmt', 'OPS')
+                                        ->where('nb_jour_pour_lache_quai', '>', 0)
+                                        ->where('nb_jour_pour_lache_mer', '>', 0)
+                                        ->count()}}</td>
                         </tr>
                     </table>
                 </div>
@@ -76,25 +92,35 @@
                     <table class='table table-hover'>
                         <tr title=''>
                             <td>Nb de marins</td>
-                            <td class='w-25'>0</td>
+                            <td class='w-25'>{{$statistiques->where('gpmt', 'NAV')->count()}}</td>
                         </tr>
                         <tr>
                             <td>Nb marins l&acirc;ch&eacute;s quai</td>
-                            <td>0</td>
+                            <td>{{ $statistiques
+                                    ->where('gpmt', 'NAV')
+                                    ->where('nb_jour_pour_lache_quai', '>', 0)
+                                    ->count() }}</td>
                         </tr>
                         <tr>
                             <td>Nb marins l&acirc;ch&eacute;s mer</td>
-                            <td>0</td>
+                            <td>{{ $statistiques
+                                       ->where('gpmt', 'NAV')
+                                       ->where('nb_jour_pour_lache_mer', '>', 0)
+                                       ->count() }}</td>
                         </tr>
                         <tr>
                             <td>Nb marins l&acirc;ch&eacute;s quai+mer</td>
-                            <td>0</td>
+                            <td>{{ $statistiques
+                                        ->where('gpmt', 'NAV')
+                                        ->where('nb_jour_pour_lache_quai', '>', 0)
+                                        ->where('nb_jour_pour_lache_mer', '>', 0)
+                                        ->count()}}</td>
                         </tr>
                     </table>
                 </div>
             </div>
             <div id='durmarin' class='card bg-light ml-3 w-100' style='display:none;'>
-                <div class='card-header'>Dur&eacute;e moyenne (jour) avant l&acirc;cher par marin</div>
+                <div class='card-header'>Dur&eacute;e moyenne avant l&acirc;cher par marin</div>
                 <table class='table table-hover'>
                     <tr>
                         <td>Service</td>
@@ -108,10 +134,24 @@
                         <td>Pr&eacute;sence</td>
                         <td>Date d&eacute;b.</td>
                     </tr>
+                    @foreach ($statistiques as $stat)
+                    <tr>
+                        <td>{{$stat->service}}</td>
+                        <td>{{$stat->grade}}</td>
+                        <td>{{$stat->diplome}}</td>
+                        <td>{{$stat->specialite}}</td>
+                        <td>{{$stat->name}}</td>
+                        <td>{{$stat->prenom}}</td>
+                        <td>{{$stat->nb_jour_pour_lache_quai}}</td>
+                        <td>{{$stat->nb_jour_pour_lache_mer}}</td>
+                        <td>{{$stat->nb_jour_gtr}}</td>
+                        <td>{{$stat->date_debarq}}</td>
+                    </tr>
+                    @endforeach
                 </table>
             </div>
             <div id='durspe' class='card bg-light ml-3 w-50' style='display:none;'>
-                <div class='card-header'>Dur&eacute;e moyenne (jour) avant l&acirc;cher par brevet/sp&eacute;cialit&eacute; (du <b>2021-07-31</b> au <b>2022-07-31</b>)</div>
+                <div class='card-header'>Dur&eacute;e moyenne (jour) avant l&acirc;cher par brevet/sp&eacute;cialit&eacute; </div>
                 <table class='table table-hover'>
                     <tr>
                         <td>Brevet</td>
@@ -120,10 +160,28 @@
                         <td>Fonction mer</td>
                         <td>Pr&eacute;sence</td>
                     </tr>
+                    @foreach($statistiques->pluck('diplome', 'specialite')->unique() as $spe => $brevet)
+                    <tr>
+                        <td>{{$brevet}}</td>
+                        <td>{{$spe}}</td>
+                        <td>{{$statistiques
+                                    ->where('specialite', $spe)
+                                    ->where('diplome', $brevet)
+                                    ->avg('nb_jour_pour_lache_quai')}}</td>
+                        <td>{{$statistiques
+                                    ->where('specialite', $spe)
+                                    ->where('diplome', $brevet)
+                                    ->avg('nb_jour_pour_lache_mer')}}</td>
+                        <td>{{$statistiques
+                                    ->where('specialite', $spe)
+                                    ->where('diplome', $brevet)
+                                    ->avg('nb_jour_gtr')}}</td>
+                    </tr>
+                    @endforeach
                 </table>
             </div>
             <div id='durgtr' class='card bg-light ml-3 w-50' style='display:none;'>
-                <div class='card-header'>Dur&eacute;e moyenne (jour) avant l&acirc;cher par brevet/sp&eacute;cialit&eacute; (du <b>2020-09-01</b> au <b>2022-08-29</b>)</div>
+                <div class='card-header'>Dur&eacute;e moyenne (jour) avant l&acirc;cher par brevet/sp&eacute;cialit&eacute; </div>
                 <table class='table table-hover'>
                     <tr>
                         <td>Brevet</td>
@@ -131,20 +189,6 @@
                         <td>Fonction quai</td>
                         <td>Fonction mer</td>
                         <td>Pr&eacute;sence</td>
-                    </tr>
-                    <tr>
-                        <td>BS</td>
-                        <td>DET</td>
-                        <td>128</td>
-                        <td>0</td>
-                        <td>165</td>
-                    </tr>
-                    <tr>
-                        <td>BAT</td>
-                        <td>SITEL</td>
-                        <td>0</td>
-                        <td>0</td>
-                        <td>65</td>
                     </tr>
                 </table>
             </div>
