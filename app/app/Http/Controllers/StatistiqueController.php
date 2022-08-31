f6f1646935630b6cc1d29865913da26f6b365196
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2022-08-31 16:59:26 +0200

    Creation de methodes du controlleur.

--- app/app/Http/Controllers/StatistiqueController.php
+++ app/app/Http/Controllers/StatistiqueController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 use App\Models\Statistique;
 
+use App\Models\Stage;
+use App\Models\User;
+use App\Models\Service;
+use App\Models\Fonction;
+
 class StatistiqueController extends Controller
 {
     public function index(Request $request) 
@@ -29,5 +34,47 @@ class StatistiqueController extends Controller
         return view('statistiques.index', ['period' => $period,
                                            'liste_des_periodes' => $liste_des_periodes,
                                            'statistiques'       => $statistiques]);
+    }
+    
+    public function pourtuteurs()
+    {
+        
+        $stages = Stage::all();
+        $users = User::local();
+        $services = Service::orderBy('service_libcourt')->get();
+        $fonctionsaquai = Fonction::where('typefonction_id', 2);
+        
+        return view('statistiques.pourtuteurs', ['stages'   => $stages,
+                                   'services' => $services,
+                                   'fonctionsaquai' => $fonctionsaquai,
+                                   'users'    => $users]);
+    }
+    
+    public function pourem()
+    {
+        
+        $stages = Stage::all();
+        $users = User::local();
+        $services = Service::orderBy('service_libcourt')->get();
+        $fonctionsaquai = Fonction::where('typefonction_id', 2);
+        
+        return view('statistiques.pourem', ['stages'   => $stages,
+                                   'services' => $services,
+                                   'fonctionsaquai' => $fonctionsaquai,
+                                   'users'    => $users]);
+    }
+    
+    public function pour2ps()
+    {
+        
+        $stages = Stage::all();
+        $users = User::local();
+        $services = Service::orderBy('service_libcourt')->get();
+        $fonctionsaquai = Fonction::where('typefonction_id', 2);
+        
+        return view('statistiques.pour2ps', ['stages'   => $stages,
+                                   'services' => $services,
+                                   'fonctionsaquai' => $fonctionsaquai,
+                                   'users'    => $users]);
     }
 }
