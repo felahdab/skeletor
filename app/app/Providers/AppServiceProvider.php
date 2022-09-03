9770965d942be5859f5b0a4605cf83f745f10d20
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2022-09-04 00:26:54 +0200

    Make the HTTPS constraint dependent on environnement.

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -25,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        \Illuminate\Support\Facades\URL::forceScheme('https');
+        
+        if (env('APP_ENV') == 'production')
+            \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 }
