4e0d1b127d1bbd4afe33641b455a2c5027319713
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2022-09-04 21:44:56 +0200

    Created livewire component to list fonctions.

---
+++ app/resources/views/livewire/fonction-list.blade.php
@@ -0,0 +1,43 @@
+<div>
+    <input wire:model='filter' type="text" placeholder="Filtrer...">
+
+    <div wire:loading>
+        Chargement...
+    </div>
+
+    <div wire:loading.remove>
+
+         <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col" width="1%">#</th>
+                <th scope="col" width="8%">Libelle court</th>
+                <th scope="col">Libelle long</th>
+                <th scope="col" width="1%" colspan="3"></th>    
+            </tr>
+            </thead>
+            <tbody>
+                @foreach($fonctions as $fonction)
+                    <tr>
+                        <th scope="row">{{ $fonction->id }}</th>
+                        <td>{{ $fonction->fonction_libcourt }}</td>
+                        <td>{{ $fonction->fonction_liblong }}</td>
+                        <td><a href="{{ route('fonctions.show', $fonction->id) }}" class="btn btn-primary btn-sm">Consulter</a></td>
+                        <td><a href="{{ route('fonctions.edit', $fonction->id) }}" class="btn btn-info btn-sm">Editer</a></td>
+                        @can('fonctions.destroy')
+                        <td>
+                            {!! Form::open(['method' => 'DELETE','route' => ['fonctions.destroy', $fonction->id],'style'=>'display:inline']) !!}
+                            {!! Form::submit('Supprimer', ['class' => 'btn btn-danger btn-sm']) !!}
+                            {!! Form::close() !!}
+                        </td>
+                        @endcan
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <div class="d-flex">
+            {!! $fonctions->links() !!}
+        </div>
+    </div>
+</div>
