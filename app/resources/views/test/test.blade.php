67a0ed03195e990a7f82a8e6d87055f8f7d0884d
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2022-09-04 21:23:03 +0200

    Just for testing.

--- app/resources/views/test/test.blade.php
+++ app/resources/views/test/test.blade.php
@@ -1,13 +1,6 @@
-<html>
-<head>
-    <title> Test LiveWire </title>
-    @livewireStyles
-</head>
-<body>
-    Hello World
-    
-    @livewire('counter')
-    
-    @livewireScripts
-</body>
-</html>
+@extends('layouts.app-master')
+
+@section('content')
+    @livewire('user-list')
+@endsection
+
