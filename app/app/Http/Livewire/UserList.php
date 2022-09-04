9bac127d2092fcd4c09ba6e3f328d7e41acc7dfe
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2022-09-04 21:22:00 +0200

    Outragously simple user list with basic search functionnality.

---
+++ app/app/Http/Livewire/UserList.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Livewire\WithPagination;
+
+use App\Models\User;
+
+class UserList extends Component
+{
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+    
+    public $filter="";
+    
+    public function render()
+    {
+    
+        return view('livewire.user-list', [
+            'users' => User::local()->where('name', 'like', '%'. $this->filter . '%')->paginate(10),
+        ]);
+    }
+}
---
+++ app/resources/views/livewire/user-list.blade.php
@@ -0,0 +1,43 @@
+<div>
+    <input wire:model='filter' type="text" placeholder="Filtrer...">
+
+    <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col" width="1%">#</th>
+                <th scope="col" width="8%">Nom</th>
+                <th scope="col" width="8%">Prénom</th>
+                <th scope="col">Email</th>
+                <th scope="col" width="10%">Roles</th>
+                <th scope="col" width="1%" colspan="3"></th>    
+            </tr>
+            </thead>
+            <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <th scope="row">{{ $user->id }}</th>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->prenom }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>
+                            @foreach($user->roles as $role)
+                                <span class="badge bg-primary">{{ $role->name }}</span>
+                            @endforeach
+                        </td>
+                        <td><a href="{{ route('users.show', $user->id) }}" class="btn btn-warning btn-sm">Afficher</a></td>
+                        <td><a href="{{ route('users.edit', $user->id) }}" class="btn btn-info btn-sm">Editer</a></td>
+                        <td>
+                            {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
+                            {!! Form::submit('Supprimer', ['class' => 'btn btn-danger btn-sm']) !!}
+                            {!! Form::close() !!}
+                        </td>
+                        @can('changepasswd.allusers')
+                        <td><a href="{{ route('changepasswd.show', $user->id) }}" class="btn btn-secondary btn-sm">Changer le mot de passe</a></td>
+                        @endcan
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        
+        {{ $users->links() }}
+</div>
