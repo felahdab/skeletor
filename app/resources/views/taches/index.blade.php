7dad738b01bcca06fee7dec6de75eace7aec02b2
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2022-09-04 22:00:55 +0200

    Converted Tache index to livewire.

--- app/resources/views/taches/index.blade.php
+++ app/resources/views/taches/index.blade.php
@@ -12,46 +12,6 @@
         <div class="mt-2">
             @include('layouts.partials.messages')
         </div>
-
+        @livewire('tache-list')
     </div>
-    
-            {!! Form::open(['method' => 'GET','route' => request()->route()->getName()]) !!}
-            {!! Form::text('filter', $filter) !!}
-            {!! Form::submit('Filtrer', ['class' => 'btn btn-primary btn-sm']) !!}
-            {!! Form::close() !!}
-    
-            {{ $taches->count() }} taches
-            <table class="table table-striped">
-            <thead>
-            <tr>
-                <th scope="col" width="1%">#</th>
-                <th scope="col" width="8%">Libelle court</th>
-                <th scope="col">Libelle long</th>
-                <th scope="col" width="1%" colspan="3"></th>    
-            </tr>
-            </thead>
-            <tbody>
-                @foreach($taches as $tache)
-                    <tr>
-                        <th scope="row">{{ $tache->id }}</th>
-                        <td>{{ $tache->tache_libcourt }}</td>
-                        <td>{{ $tache->tache_liblong }}</td>
-                        <td><a href="{{ route('taches.show', $tache->id) }}" class="btn btn-primary btn-sm">Consulter</a></td>
-                        <td><a href="{{ route('taches.edit', $tache->id) }}" class="btn btn-info btn-sm">Editer</a></td>
-                        @can('taches.destroy')
-                        <td>
-                            {!! Form::open(['method' => 'DELETE','route' => ['taches.destroy', $tache->id],'style'=>'display:inline']) !!}
-                            {!! Form::submit('Supprimer', ['class' => 'btn btn-danger btn-sm']) !!}
-                            {!! Form::close() !!}
-                        </td>
-                        @endcan
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-        <div class="d-flex">
-            {!! $taches->withQueryString()->links() !!}
-        </div>
-
 @endsection
