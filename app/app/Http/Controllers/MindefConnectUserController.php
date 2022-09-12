b073ccc53f20ae7f0ef168fa08dee6090f81d3ed
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-09-12 13:51:38 +0200

    Created Model and associated classes for Mindef Connect User

---
+++ app/app/Http/Controllers/MindefConnectUserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMindefConnectUserRequest;
+use App\Http\Requests\UpdateMindefConnectUserRequest;
+use App\Models\MindefConnectUser;
+
+class MindefConnectUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreMindefConnectUserRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreMindefConnectUserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MindefConnectUser  $mindefConnectUser
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MindefConnectUser $mindefConnectUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MindefConnectUser  $mindefConnectUser
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MindefConnectUser $mindefConnectUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateMindefConnectUserRequest  $request
+     * @param  \App\Models\MindefConnectUser  $mindefConnectUser
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateMindefConnectUserRequest $request, MindefConnectUser $mindefConnectUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MindefConnectUser  $mindefConnectUser
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MindefConnectUser $mindefConnectUser)
+    {
+        //
+    }
+}
