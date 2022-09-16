1008ce3a7429f76d58709ed00b6c08aea6943a3c
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-09-16 10:38:38 +0200

    Again

--- ffast-stack/up.sh
+++ ffast-stack/up.sh
@@ -6,3 +6,8 @@ STACKNAME=$(basename $PARENTDIR)
 echo $STACKNAME
 
 docker compose -p $STACKNAME up -d
+
+echo "Waiting 10 seconds before initializing database and seeding data."
+sleep 10
+docker exec -it ${STACKNAME}-php-1 ./artisan migrate --force
+docker exec -it ${STACKNAME}-php-1 ./artisan db:seed --force
