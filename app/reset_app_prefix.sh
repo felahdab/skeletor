c0db2656a8c42a9053af0831e3a33db2e8d1fc2c
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-09-19 17:10:05 +0200

    Made the reset app aprefix script independant of calling directory

--- app/reset_app_prefix.sh
+++ app/reset_app_prefix.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+SCRIPTDIRECTORY="$(dirname "$0")"
+BASEDIRECTORY=$(realpath $SCRIPTDIRECTORY)
+cd $BASEDIRECTORY
+
+PARENTDIR=$(dirname $BASEDIRECTORY)
+STACKNAME=$(basename $PARENTDIR)
+echo $BASEDIRECTORY
+echo $STACKNAME
 
 sed -i 's/APP_PREFIX=.*/APP_PREFIX=""/g' .env
 
