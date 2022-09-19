35ebb9bbc1ecb7c279cddba30e0ae15bd2eea28b
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-09-19 16:55:14 +0200

    Adjusted all scripts.

--- app/adjust_permissions.sh
+++ app/adjust_permissions.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 SCRIPTDIRECTORY="$(dirname "$0")"
 BASEDIRECTORY=$(realpath $SCRIPTDIRECTORY)
-
-
 cd $BASEDIRECTORY
+
 chown -R :ffastdev *
 chown -R :ffastdev .*
 chmod -R g+rwx .
--- ffast-stack/down.sh
+++ ffast-stack/down.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+SCRIPTDIRECTORY="$(dirname "$0")"
+BASEDIRECTORY=$(realpath $SCRIPTDIRECTORY)
+cd $BASEDIRECTORY
 
-CURRENTDIR=$(pwd)
-PARENTDIR=$(dirname $CURRENTDIR)
+PARENTDIR=$(dirname $BASEDIRECTORY)
 STACKNAME=$(basename $PARENTDIR)
+echo $BASEDIRECTORY
 echo $STACKNAME
 
 docker compose -p $STACKNAME down 
