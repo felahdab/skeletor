86820a95929d9502f90abed99b6421eff1abc32f
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-09-20 16:08:09 +0200

    typo, mise en forme et ajout à la saisie des champs manquants de la table

--- app/resources/views/fonctions/create.blade.php
+++ app/resources/views/fonctions/create.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="bg-light p-4 rounded">
         <h2>Fonctions</h2>
+        <div style='text-align:right;'>* champs obligatoires </div>
         <div class="mt-2">
             @include('layouts.partials.messages')
         </div>
@@ -18,13 +19,41 @@
                     </div>
                 </div>
                 <div class='form-group row' >
-                    <label for='fonction[fonction_liblong]' class='col-sm-5 col-form-label'>Libell&eacute; long</label>
+                    <label for='fonction[fonction_liblong]' class='col-sm-5 col-form-label'>Libell&eacute; long *</label>
                     <div class='col-sm-5'>
                         <input type='text' class='form-control' name='fonction[fonction_liblong]' id='fonction[fonction_liblong]' placeholder='Libell&eacute; long' value="" >
                     </div>
                 </div>
+                <div class="form-group row">
+                    <label for="fonction[typefonction_id]" class="col-sm-5 col-form-label">Type de fonction *</label>
+                    <div class='col-sm-5'>
+                        <select class="form-control" 
+                            name="fonction[typefonction_id]" required>                        
+                            @foreach($typefonctions as $typefonction)
+                                <option value="{{ $typefonction->id }}" >
+                                    {{ $typefonction->typfonction_libcourt }}
+                                    {{ $typefonction->id == 1
+                                        ? ' (par défaut)'
+                                        : '' }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class='form-group row' >
+                    <label for='fonction_lache' class='col-sm-5 col-form-label'>Lâcher</label>
+                    <div class='col-sm-5'>
+                        <input type='checkbox'name='fonction[fonction_lache]' id='fonction[fonction_lache]'> 
+                    </div>
+                </div>
+                <div class='form-group row' >
+                    <label for='fonction_double' class='col-sm-5 col-form-label'>Double</label>
+                    <div class='col-sm-5'>
+                        <input type='checkbox'name='fonction[fonction_double]' id='fonction[fonction_double]'> 
+                    </div>
+                </div>                
                 <div>
-                    <button class='btn btn-primary w-100 mt-4' type='submit' id='btnmodifobj' name='btnmodifobj'>Créer</button>
+                    <button class='btn btn-primary mt-4' type='submit' id='btnmodifobj' name='btnmodifobj'>Créer</button>
                     <br>&nbsp;
                 </div>
             </div>
