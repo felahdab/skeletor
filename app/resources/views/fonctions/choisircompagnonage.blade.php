7b8bf8ea2dce5df319d72a560285ab2c5af08428
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-09-21 16:12:35 +0200

    SZA: commit inutile 3

--- app/resources/views/fonctions/choisircompagnonage.blade.php
+++ app/resources/views/fonctions/choisircompagnonage.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app-master')
 
 @section('content')
+
     <div class="bg-light p-4 rounded">
         <h2>Fonctions</h2>
         <div class='lead'>Ajout d'un compagnonnage pour la fonction : {!!$fonction->fonction_libcourt !!} </div>
