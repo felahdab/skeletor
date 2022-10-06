70785900308f887deae4f0e4d4902d3feb873854
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-10-06 15:34:23 +0200

    Updated image for IP link

--- app/database/seeders/LiensSeeder.php
+++ app/database/seeders/LiensSeeder.php
@@ -20,7 +20,7 @@ class LiensSeeder extends Seeder
             [1, 'WikiFREMM', 'https://wiki-gtr-brest.intradef.gouv.fr/doku.php?id=Accueil', 'wikiFREMM.png'],
             [2, 'GTR/T', 'https://portail-gtr-toulon.intradef.gouv.fr/index.php', 'gtr-toulon.png'],
             [3, 'GTR/B', 'http://portail-gtr-brest.marine.defense.gouv.fr/', 'gtr-brest.png'],
-            [4, 'IP', 'https://wiki-gtr-brest.intradef.gouv.fr/doku.php?id=00-page_de_garde:instructions_permanentes', 'gtr-brest.png'],
+            [4, 'IP', 'https://wiki-gtr-brest.intradef.gouv.fr/doku.php?id=00-page_de_garde:instructions_permanentes', 'InsigneEscouade.jpg'],
             [5, 'FREMM360', 'http://pem-form.intradef.gouv.fr/atrium/Vv_Fremm/vvirtuelle.html', 'fremm360.png'],
             ];
         foreach ($records as $record){
