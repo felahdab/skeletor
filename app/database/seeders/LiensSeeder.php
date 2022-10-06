b9cc9d65a8ec6f173e78ee3a469fd8847f21005e
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-10-06 14:26:01 +0200

    Included images. Updated Seeder. Deleted unused controller.

--- app/database/seeders/LiensSeeder.php
+++ app/database/seeders/LiensSeeder.php
@@ -17,12 +17,12 @@ class LiensSeeder extends Seeder
     public function run()
     {
         $records = [
-            [1, 'WikiFREMM', 'https://wiki-gtr-brest.intradef.gouv.fr/doku.php?id=Accueil', 'a voir',2],
-            [2, 'GTR/T', 'https://portail-gtr-toulon.intradef.gouv.fr/index.php', 'a voir',2],
-            [3, 'IP', 'https://wiki-gtr-brest.intradef.gouv.fr/doku.php?id=00-page_de_garde:instructions_permanentes', 'a voir',2],
-            [4, 'FREMM360', 'http://pem-form.intradef.gouv.fr/atrium/Vv_Fremm/vvirtuelle.html', 'a voir',2],
-            [5, 'WikiFREMM', 'https://wiki-gtr-brest.intradef.gouv.fr/doku.php?id=Accueil', 'a voir',1],
-            [6, 'FREMM360', 'http://pem-form.intradef.gouv.fr/atrium/Vv_Fremm/vvirtuelle.html', 'a voir',1],
+            [1, 'WikiFREMM', 'https://wiki-gtr-brest.intradef.gouv.fr/doku.php?id=Accueil', 'wikiFREMM.png',2],
+            [2, 'GTR/T', 'https://portail-gtr-toulon.intradef.gouv.fr/index.php', 'gtr-toulon.png',2],
+            [3, 'IP', 'https://wiki-gtr-brest.intradef.gouv.fr/doku.php?id=00-page_de_garde:instructions_permanentes', 'gtr-brest.png',2],
+            [4, 'FREMM360', 'http://pem-form.intradef.gouv.fr/atrium/Vv_Fremm/vvirtuelle.html', 'fremm360.png',2],
+            [5, 'WikiFREMM', 'https://wiki-gtr-brest.intradef.gouv.fr/doku.php?id=Accueil', 'wikiFREMM.png',1],
+            [6, 'FREMM360', 'http://pem-form.intradef.gouv.fr/atrium/Vv_Fremm/vvirtuelle.html', 'fremm360.png',1],
             ];
         foreach ($records as $record){
             DB::insert('insert into liens (id, lien_lib, lien_url, lien_image ,unite_id) values (?, ?, ?, ?, ?)', $record);
