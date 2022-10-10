a5233e01bf1f2f48bf31e8ffab6ae10edff73a61
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-10-10 16:03:03 +0200

    corrections fautes et présentation du livret

--- app/resources/views/fonctions/choisircompagnonage.blade.php
+++ app/resources/views/fonctions/choisircompagnonage.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <div class="bg-light p-4 rounded">
-        <h2>Fonctions</h2>
+        <h2>Compagnonnages</h2>
         <div class='lead'>Ajout d'un compagnonnage pour la fonction : {!!$fonction->fonction_libcourt !!} </div>
     </div>
     
-    @livewire('compagnonage-list', ['fonction' => $fonction, 'mode' => 'selection']);
+    @livewire('compagnonage-list', ['fonction' => $fonction, 'mode' => 'selection'])
     
 @endsection
