072fc8bb487f19798aef9d08d2207e90e73ea405
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-10-13 11:10:05 +0200

    gestion d'un user sans secteur

--- app/resources/views/statistiques/pourtuteurs.blade.php
+++ app/resources/views/statistiques/pourtuteurs.blade.php
@@ -3,14 +3,21 @@
 @section('content')
     <div class="bg-light p-4 rounded">
         <h2>SUIVI TRANSFORMATION </h2>
-        <div class="lead">
-            Liste des marins en transformation dans le service {{ $currentuser->displayService() }}
-        </div>
-        <div style="text-align: right;">
-            Cliquez sur un marin pour afficher son livret de transformation
-        </div>
-        <div class="mt-3">
-            <livewire:stattuteur-table>
-        </div>
+        
+        @if (auth()->user()->secteur_id != 0)
+            <div class="lead">
+                Liste des marins en transformation dans le service {{ $currentuser->displayService() }}
+            </div>
+            <div style="text-align: right;">
+                Cliquez sur un marin pour afficher son livret de transformation
+            </div>
+            <div class="mt-3">
+                <livewire:stattuteur-table>
+            </div>        
+        @else
+            <div class="lead">
+                Vous n'êtes pas rattaché à un service !!!
+            </div>
+        @endif
     </div>
 @endsection
