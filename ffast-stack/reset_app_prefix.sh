6014627f33031d96118b3582f2bb6c00b6d28bbe
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-10-14 15:12:39 +0200

    Updated ffast-php image generation, supervisor configuration, nginx configuration and bumping/resetting scripts to automate queue:worker start

--- ffast-stack/reset_app_prefix.sh
+++ ffast-stack/reset_app_prefix.sh
@@ -9,3 +9,4 @@ STACKNAME=$(basename $PARENTDIR)
 
 echo -n "" > .env
 sed -i '/pma/c\    location ^~ /STACKNAME/pma/ {' nginx.conf
+sed -i '/worker/c\    location ^~ /STACKNAME/worker/ {' nginx.conf
