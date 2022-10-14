2dd9721e38280b029947c7a5192e9a887b5a937f
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-10-14 15:44:29 +0200

    Corrected scripts and nginx config.

--- ffast-stack/reset_app_prefix.sh
+++ ffast-stack/reset_app_prefix.sh
@@ -9,4 +9,5 @@ STACKNAME=$(basename $PARENTDIR)
 
 echo -n "" > .env
 sed -i '/pma/c\    location ^~ /STACKNAME/pma/ {' nginx.conf
-sed -i '/worker/c\    location ^~ /STACKNAME/worker/ {' nginx.conf
+sed -i '/location.*worker/c\    location ^~ /STACKNAME/worker/ {' nginx.conf
+sed -i '/rewrite/c\        rewrite /STACKNAME/worker/(.*) /$1 break;' nginx.conf
