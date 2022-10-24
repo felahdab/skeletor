4d5fc66b7549ac0900cff1c8fb2818e13c05adbf
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-10-24 13:40:35 +0200

    Started creating Annudef views and livewire component

---
+++ app/resources/views/annudef/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app-master')
+
+@section('content')
+    
+
+    <div class="bg-light p-4 rounded">
+        <h2>Marins</h2>
+        <div class="lead">
+            Gestion des marins
+            <a href="{{ route('users.create') }}" class="btn btn-primary btn-sm float-right">Ajouter un marin</a>
+        </div>
+        <div class="mt-3">
+            <livewire:users-table mode="gestion">
+        </div>
+
+    </div>
+@endsection
