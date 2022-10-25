0a9ce1d14aa0186989d9e3149c6b5f31d4940d20
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-10-25 11:29:34 +0200

    Removed useless code

--- app/routes/web.php
+++ app/routes/web.php
@@ -110,11 +110,8 @@ Route::group(['namespace' => 'App\Http\Controllers'], function()
             Route::post('/{user}/retirerfonction', 'UsersController@retirerfonction')->name('users.retirerfonction');
             Route::get('/{user}/livret', 'TransformationController@livret')->name('transformation.livret');
             Route::get('/{user}/livretpdf', 'TransformationController@livretpdf')->name('transformation.livretpdf');
-            Route::post('/{user}/livret', 'TransformationController@updatelivret')->name('transformation.updatelivret');
-            Route::post('/{user}/validerlacheoudouble/{fonction}', 'TransformationController@validerlacheoudouble')->name('transformation.validerlacheoudouble');
             Route::get('/{user}/progression', 'TransformationController@progression')->name('transformation.progression');
             Route::get('/{user}/fichebilan', 'TransformationController@fichebilan')->name('transformation.fichebilan');
-            
             Route::get('/mafichebilan', 'TransformationController@mafichebilan')->name('transformation.mafichebilan');
             Route::get('/monlivret', 'TransformationController@monlivret')->name('transformation.monlivret');
             Route::get('/maprogression', 'TransformationController@maprogression')->name('transformation.maprogression');
