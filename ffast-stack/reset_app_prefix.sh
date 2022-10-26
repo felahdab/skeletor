58c4f5e7f1c1ab3615ed445ea692b2be40d1de7e
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2022-10-26 17:13:35 +0200

    Simplified nginx.conf reset process. Added code-editor container based on codercom/code-server image.

--- ffast-stack/reset_app_prefix.sh
+++ ffast-stack/reset_app_prefix.sh
@@ -8,6 +8,4 @@ STACKNAME=$(basename $PARENTDIR)
 
 
 echo -n "" > .env
-sed -i '/pma/c\    location ^~ /STACKNAME/pma/ {' nginx.conf
-sed -i '/location.*worker/c\    location ^~ /STACKNAME/worker/ {' nginx.conf
-sed -i '/rewrite/c\        rewrite /STACKNAME/worker/(.*) /$1 break;' nginx.conf
+cp -a nginx_base.conf nginx.conf
