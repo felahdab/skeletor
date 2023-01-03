5c2e938a8b1611d700120dcc67f023428b42874a
Sandrine <sandrine.zambelli@intradef.gouv.fr>
2023-01-03 11:04:55 +0100

    Corrected details for testing integration

--- app/app/View/Components/DocumentationLink.php
+++ app/app/View/Components/DocumentationLink.php
@@ -7,14 +7,18 @@ use Illuminate\View\Component;
 class DocumentationLink extends Component
 {
 	public $page;    
+    public $version;
 	/**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct($page='')
+    public function __construct($page='', $version=null)
     {
         $this->page = $page;
+        if ($version == null)
+            $version= env('DOC_VERSION');
+        $this->version = $version;
     }
 
     /**
--- app/resources/views/components/documentation-link.blade.php
+++ app/resources/views/components/documentation-link.blade.php
@@ -1,3 +1,3 @@
 <div>
-   <a href="{{ route('larecipe.show', ['version' => env('DOC_VERSION'), 'page' => $page]) }}"><img src='{!! asset("assets/images/Question_mark.svg") !!}' style="height:40px;"></a>
+   <a href="{{ route('larecipe.show', ['version' => $version, 'page' => $page]) }}"><img src='{!! asset("assets/images/Question_mark.svg") !!}' style="height:40px;"></a>
 </div>
