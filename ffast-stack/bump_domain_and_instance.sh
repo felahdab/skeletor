daf16eff01aa69c17debfacf8aa27eddf1219375
Sandrine <sandrine.zambelli@intradef.gouv.fr>
2023-01-30 15:53:29 +0100

    Added label definition in stack for traefik integration

--- ffast-stack/bump_domain_and_instance.sh
+++ ffast-stack/bump_domain_and_instance.sh
@@ -12,5 +12,7 @@ source $PARENTDIR/.env
 ./reset_app_prefix.sh
 
 echo "INSTANCEURL=https://$DOMAIN/$PREFIX" > .env
+echo "PREFIX=$PREFIX" >> .env
+echo "DOMAIN=$DOMAIN" >> .env
 SED_CMD='s/STACKNAME/'${PREFIX}'/g'
 sed $SED_CMD nginx_${ENVIRONNEMENT}.conf > nginx.conf
