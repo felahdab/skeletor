26d9f83bb22af88052d0c8e59fe802577573ccaf
Sandrine <sandrine.zambelli@intradef.gouv.fr>
2023-02-09 11:20:05 +0100

    modif table hitoriqur pour rendre valeur null possible pour les champs userid

--- database/migrations/2023_02_09_063953_enrich_transformation_histories.php
+++ database/migrations/2023_02_09_063953_enrich_transformation_histories.php
@@ -16,6 +16,10 @@ return new class extends Migration
         Schema::table('transformation_histories', function (Blueprint $table) {
             $table->string('modifying_user', 500)->nullable(true);
             $table->string('modified_user', 500)->nullable(true);
+        }
+        Schema::table('transformation_histories', function (Blueprint $table) {
+            $table->string('modifying_user_id')->nullable(true)->change();
+            $table->string('modified_user_id')->nullable(true)->change();
         });
     }
     /**
@@ -28,5 +32,9 @@ return new class extends Migration
         Schema::table('transformation_histories', function (Blueprint $table) {
             $table->dropColumn('modifying_user');
             $table->dropColumn('modified_user');
+        }
+        Schema::table('transformation_histories', function (Blueprint $table) {
+            $table->string('modifying_user_id')->nullable(false)->change();
+            $table->string('modified_user_id')->nullable(false)->change();
         });
     }};
