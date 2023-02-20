a73a551c0c220efd2bc4c893d9021bdd1f8f0e7e
Sandrine <sandrine.zambelli@intradef.gouv.fr>
2023-02-20 10:30:26 +0100

    Finalise la migration qui transforme la table d'historique pour retirer les user_id et mettre les displayname a la place.

--- database/migrations/2023_02_09_063953_enrich_transformation_histories.php
+++ database/migrations/2023_02_09_063953_enrich_transformation_histories.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\User;
+use App\Models\TransformationHistory;
+
 return new class extends Migration
 {
     /**
@@ -16,10 +19,19 @@ return new class extends Migration
         Schema::table('transformation_histories', function (Blueprint $table) {
             $table->string('modifying_user', 500)->nullable(true);
             $table->string('modified_user', 500)->nullable(true);
-        }
+        });
+
+        foreach(TransformationHistory::all() as $enreg){
+            $enreg->modifying_user = User::find($enreg->modifying_user_id)?->display_name;
+            $enreg->modified_user = User::find($enreg->modified_user_id)?->display_name;
+            $enreg->save();
+        };
+
         Schema::table('transformation_histories', function (Blueprint $table) {
-            $table->string('modifying_user_id')->nullable(true)->change();
-            $table->string('modified_user_id')->nullable(true)->change();
+            $table->dropForeign(['modifying_user_id']);
+            $table->dropForeign(['modified_user_id']);
+            $table->dropColumn('modifying_user_id');
+            $table->dropColumn('modified_user_id');
         });
     }
     /**
@@ -32,9 +44,10 @@ return new class extends Migration
         Schema::table('transformation_histories', function (Blueprint $table) {
             $table->dropColumn('modifying_user');
             $table->dropColumn('modified_user');
-        }
+        });
         Schema::table('transformation_histories', function (Blueprint $table) {
             $table->string('modifying_user_id')->nullable(false)->change();
             $table->string('modified_user_id')->nullable(false)->change();
         });
-    }};
+    }
+};
