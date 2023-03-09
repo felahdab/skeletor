0610f775380bf50d14b065f0dc31e282bbeaf8de
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2023-03-09 11:32:33 +0100

    Add link to dashboard page in the navbar.

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -77,6 +77,7 @@
                 @can('statistiques.pour2ps')<a class="dropdown-item" href="{{route('statistiques.pour2ps')}}">Bilan par stage</a>@endcan
                 @can('statistiques.pourem')<a class="dropdown-item" href="{{route('statistiques.pourem')}}">Bilan global</a>@endcan
                 @can('statistiques.index')<a class="dropdown-item" href="{{route('statistiques.index')}}">Indicateurs</a>@endcan
+                @can('statistiques.dashboard')<a class="dropdown-item" href="{{route('statistiques.dashboard')}}">Tableau de bord</a>@endcan
                 
               </div>
             </div>
