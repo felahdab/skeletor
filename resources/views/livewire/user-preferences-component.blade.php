eb509171325d40d500e55ab0e07c61fe11f31731
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2023-04-05 10:48:11 +0200

    Ajuste le composant de gestion des preferences utilisateur pour eviter que l'accordeon se referme tout seul quand l'utilisateur change un parametre

--- resources/views/livewire/user-preferences-component.blade.php
+++ resources/views/livewire/user-preferences-component.blade.php
@@ -1,9 +1,11 @@
 <div x-data="{'daily_fonctions'  : false,
             'weekly_fonctions'   : false,
+            'fonctions_collapse' : $wire.entangle('fonctions_collapse'),
             'selected_fonctions' : [],
             'daily_services'     : false,
             'weekly_services'    : false,
-            'selected_services'  : []}">
+            'selected_services'  : [],
+            'services_collapse'  : $wire.entangle('services_collapse')}">
     <h2> Préférences </h2>
 
     <h3> Notifications </h3>
@@ -20,15 +22,21 @@
     <div class="accordion">
         <div class="accordion-item">
             <div class="accordion-header bg-primary">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordeon_fonctions">
+                <button class="accordion-button" 
+                        x-on:click="fonctions_collapse = ! fonctions_collapse"
+                        :class="fonctions_collapse ? 'collapsed': ''" 
+                        type="button" 
+                        data-bs-toggle="collapse" 
+                        data-bs-target="#accordeon_fonctions">
                     <h3> Fonctions </h3>
                 </button>
             </div>
-            <div id="accordeon_fonctions" class="accordion-collapse collapse">
+            <div id="accordeon_fonctions" class="accordion-collapse" :class="fonctions_collapse ? 'collapse': 'show'">
                 <div  class="accordion-body">
                     @foreach($fonctions as $fonction)
                     <div>           
-                        <input type='checkbox' 
+                        <input type='checkbox'
+                        x-on:click="services_collapse=true; fonctions_collapse=false"
                         wire:model="settings.ffast.notifications.pour_fonctions.liste_fonctions.{{ $fonction->id }}"
                             value="{{$fonction->id}}">
                         {{ $fonction->fonction_libcourt }}
@@ -52,15 +60,21 @@
     <div class="accordion">
         <div class="accordion-item">
             <div class="accordion-header bg-primary">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordeon_services">
+                <button class="accordion-button" 
+                    x-on:click="services_collapse = ! services_collapse"
+                    :class="services_collapse ? 'collapsed': ''" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#accordeon_services">
                     <h3> Services </h3>
                 </button>
             </div>
-            <div id="accordeon_services" class="accordion-collapse collapse">
+            <div id="accordeon_services" class="accordion-collapse" :class="services_collapse ? 'collapse': 'show'">
                 <div  class="accordion-body">
                     @foreach($services as $service)
                     <div>
                         <input type='checkbox' 
+                            x-on:click="services_collapse=false; fonctions_collapse=true"
                             wire:model="settings.ffast.notifications.pour_services.liste_services.{{ $service->id }}"
                             value="{{$service->id}}" />
                         {{ $service->service_libcourt }}
