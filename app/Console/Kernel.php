5226639755d7a0c5381c3abf264def96fd7eb1ee
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2023-04-25 11:51:39 +0000

    Cree une tache pour corriger les display_name des utilisateurs. Planifie cette tache toutes les 5 minutes.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+use App\Jobs\AdjustDisplayNames;
+
 class Kernel extends ConsoleKernel
 {
     /**
@@ -16,6 +18,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('queue:prune-batches')->hourly();
+        $schedule->job(new AdjustDisplayNames)->everyFiveMinutes();
     }
 
     /**
