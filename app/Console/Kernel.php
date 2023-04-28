50a7d8151d505a026687a1a2431c074bd4437d32
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2023-04-28 13:28:23 +0000

    Passage à un déclenchement quotidien

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('queue:prune-batches')->hourly();
-        $schedule->job(new AdjustDisplayNames)->everyFiveMinutes();
+        $schedule->job(new AdjustDisplayNames)->daily();
     }
 
     /**
