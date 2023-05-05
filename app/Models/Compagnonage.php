b8db4010103d2631e439d31d019a36c23fd1260f
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2023-05-05 15:03:01 +0000

    Mise en cache de la collection des sous objectifs d'un compagnonnage.

--- app/Models/Compagnonage.php
+++ app/Models/Compagnonage.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 use App\Models\Tache;
 use App\Models\Fonction;
@@ -24,11 +25,31 @@ class Compagnonage extends Model
     
     public function coll_sous_objectifs()
     {
+        $result = Cache::remember($this->cacheKey() . ':coll_sous_objectifs', 60*5, function () {
+            $coll = collect([]);
+            foreach ($this->taches as $tache)
+            {
+                $coll = $coll->concat($tache->coll_sous_objectifs());
+            }
+            return $coll;
+        });
+        return $result;
+
         $coll = collect([]);
-        foreach ($this->taches()->get() as $tache)
+        foreach ($this->taches as $tache)
         {
             $coll = $coll->concat($tache->coll_sous_objectifs());
         }
         return $coll;
+    }
+
+    public function cacheKey()
+    {
+        return sprintf(
+            "%s/%s-%s",
+            $this->getTable(),
+            $this->getKey(),
+            $this->updated_at->timestamp
+        );
     }
 }
