224632f63cc16dd19bff7bd2cc5842ac42766770
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2023-05-13 13:05:43 +0000

    La panneau des notifications affiche les notifications sous forme de toaster. Lorque l'on ferme une notification, celle-ci est marquée comme lue en base.

--- app/Http/Livewire/NotificationPanel.php
+++ app/Http/Livewire/NotificationPanel.php
@@ -6,8 +6,24 @@ use Livewire\Component;
 
 class NotificationPanel extends Component
 {
+    public $showPanel=false;
+
+    protected $listeners = ['$refresh'];
+
     public function render()
     {
         return view('livewire.notification-panel');
+    }
+
+    public function markNotificationAsRead($notification_id)
+    {
+        $this->showPanel=true;
+
+        foreach (auth()->user()->unreadNotifications as $notification) {
+            if ($notification->id == $notification_id) {
+                $notification->markAsRead();
+                $this->emitSelf('$refresh');
+            }
+        }
     }
 }
