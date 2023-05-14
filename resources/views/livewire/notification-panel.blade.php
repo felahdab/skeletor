37b798140e8e48a7ddfede2d4e6b5580fd759f22
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2023-05-14 07:57:26 +0000

    Modification du panneau des notifications pour utiliser les NotificationToaster. Création d'un evenement javascript pour fermer toutes les notifications.

--- resources/views/livewire/notification-panel.blade.php
+++ resources/views/livewire/notification-panel.blade.php
@@ -1,26 +1,15 @@
-<div>
+<div x-data="{}">
   @auth
-  <div class="offcanvas offcanvas-end @if($showPanel) show @endif" tabindex="-1" data-bs-scroll="true"
+  <div class="offcanvas offcanvas-end" tabindex="-1" data-bs-scroll="true"
     id="notificationPanel" aria-labelledby="offcanvasRightLabel">
     <div class="offcanvas-header">
       <h5 class="offcanvas-title" id="offcanvasRightLabel">Notifications</h5>
+      <button class="btn btn-primary btn-sm" x-on:click="$dispatch('markallasread')">Marquer tout comme vu</button>
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
       @foreach(auth()->user()->unreadNotifications as $notification)
-      <div class="toast fade show" role="alert" id="notification.{{ $notification->id }}" aria-live="assertive"
-        aria-atomic="true" x-init="$el.addEventListener('hidden.bs.toast', () => {
-              $wire.markNotificationAsRead('{{ $notification->id }}')
-            })">
-        <div class="toast-header">
-          <strong class="me-auto">{{ $notification->data["title"] }}</strong>
-          @if(array_key_exists("marin", $notification->data))<small>{{ $notification->data["marin"] }}</small> @endif
-          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-        <div class="toast-body">
-          {{ $notification->data["body"] }}
-        </div>
-      </div>
+        <livewire:notification-toaster :notification="$notification">
       @endforeach
     </div>
   </div>
