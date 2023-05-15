fd0e92df2729794779176faf5b0d6fd410e80627
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2023-05-15 19:05:30 +0000

    Rename public property of bootstrap-icon component.

--- app/View/Components/BootstrapIcon.php
+++ app/View/Components/BootstrapIcon.php
@@ -11,17 +11,16 @@ class BootstrapIcon extends Component
     /**
      * Create a new component instance.
      */
-    public function __construct(public string $icon_name='0-circle-fill.svg')
+    public function __construct(public string $iconname = '0-circle-fill.svg')
     {
-        
     }
 
     /**
      * Get the view / contents that represent the component.
      */
-    public function render(): View|Closure|string
+    public function render(): string
     {
-        $filepath = base_path("vendor/twbs/bootstrap-icons/icons/" . $this->icon_name);
+        $filepath = base_path("vendor/twbs/bootstrap-icons/icons/" . $this->iconname);
         return file_get_contents($filepath);
     }
 }
