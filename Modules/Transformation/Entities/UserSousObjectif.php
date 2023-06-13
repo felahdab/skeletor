0236156a9a6eef8a278b6d34eb4779a97de333f5
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2023-06-13 13:53:04 +0000

    Déplacement du modèle UserSousObjectif vers le module Transformation

--- Modules/Transformation/Entities/UserSousObjectif.php
+++ Modules/Transformation/Entities/UserSousObjectif.php
@@ -1,13 +1,15 @@
 <?php
 
-namespace App\Models;
+namespace Modules\Transformation\Entities;
 
+use Modules\Transformation\Traits\HasTablePrefix;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UserSousObjectif extends Model
 {
     use HasFactory;
+    use HasTablePrefix;
     
     protected $table = 'user_sous_objectif';
 
