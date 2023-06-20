21fa02e8a7010fed91ab18189dc2e0025ad66531
sandrine <sandrine.zambelli@intradef.gouv.fr>
2023-06-20 13:13:27 +0000

    nouvelle présentation pour les logins

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,41 +5,52 @@
 @endsection
 
 @section('content')
-    <div class="mt-5 mb-3">
-        <img src='{!! asset("assets/images/logo_FFAST_bleu.png") !!}' alt="Logo FFAST" style="height:250px; display: block; margin-left:auto; margin-right: auto; ">
-    </div>
-    <div>&nbsp;</div>
-    <div class="d-flex flex-row justify-content-center ">
-        <div class="p-3 me-2 card w-25" style="border: 2px solid grey; padding: 5px; box-shadow: 0px 0px 10px;">
-            <!-- partie mindefconnect -->
-            <div class="mt-2 mb-4 text-center h5">Authentification MindefConnect</div>
-            <div class="text-center">
-                <a href="{{ route('keycloak.login.redirect') }}" class="btn btn-light">
-                    <img src='{!! asset("assets/images/MDC_intradef.png") !!}' alt="Logo MDconnect" style="height:100px; width:90px">
+    <div class="d-flex justify-content-center mt-5 ">
+        <div class="card w-50 justify-content-center" >
+            <div class="w-100 text-center mt-3">
+                <div class="mb-1"><h4><b>LOGIN Automatique/DR-CPT</b></h4></div>
+                <a href="{{ route('keycloak.login.redirect') }}" class="btn btn-light w-50" >
+                    <div class="card">
+                        <div class="row g-0">
+                          <div class="col-md-4 p-2" >
+                            <img src='{!! asset("assets/images/MDC_intradef.png") !!}' alt="Logo MDconnect" style="width:70px">
+                          </div>
+                          <div class="col-md-8 border-start d-flex align-items-center">
+                                <div class="card-body ">
+                                    Se connecter
+                                </div>
+                          </div>
+                        </div>
+                      </div>
                 </a>
             </div>
-            <div class="mt-4 mb-1 text-center" style="font-size: x-small;">Cliquez sur l'image pour vous connecter automatiquement ou avec vos identifiants DR-CPT</div>
-        </div>
-        <div class="p-3 ms-2 card w-25" style="border: 2px solid grey; padding: 5px; box-shadow: 0px 0px 10px;">
-            <div class="mt-2 mb-3 text-center h5">Authentification Locale</div>
-            <!-- partie login local -->
-            <form method="post" action="{{ route('login.perform') }}">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-                <div class="mb-3">
-                    <input dusk="login-email" type="text" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email" required="required" autofocus>
-                    @if ($errors->has('username'))
-                        <span class="text-danger text-left">{{ $errors->first('username') }}</span>
-                    @endif
+            <div class="w-100 text-center mt-5 mb-5">
+              <div class="w-100" style="display: flex; justify-content: center; align-items: center;">
+                    <div style="width: 40%; height: 1px; background-color:lightgrey"></div>
+                    <span class="mx-3"><b>OU</b></span>
+                    <div style="width: 40%; height: 1px; background-color:lightgrey"></div>
                 </div>
-                <div class="mb-3">
-                    <input dusk="login-password" type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Password" required="required">
-                    @if ($errors->has('password'))
-                        <span class="text-danger text-left">{{ $errors->first('password') }}</span>
-                    @endif
-                </div>        
-                <button dusk="login-button" class="w-100 btn btn-primary" type="submit">Login</button>
-            </form>
-            <div class="mt-2 mb-1 text-center" style="font-size: x-small;">Saisissez votre adresse mail <b>complète</b> et le mot de passe défini dans l'application.</div>
+            </div>
+            <div class="w-100 text-center mb-3">
+                <h4><b>LOGIN LOCAL</b></h4>
+                <span style="font-size: small;">Saisissez votre adresse mail <b>complète</b> et le mot de passe défini dans l'application.</span>
+                <form method="post" action="{{ route('login.perform') }}">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                    <div class="m-3 d-flex justify-content-center">
+                        <input dusk="login-email" type="text" class="form-control w-75" name="email" value="{{ old('email') }}" placeholder="Email" required="required" autofocus>
+                        @if ($errors->has('username'))
+                            <span class="text-danger text-left">{{ $errors->first('username') }}</span>
+                        @endif
+                    </div>
+                    <div class="m-3 d-flex justify-content-center">
+                        <input dusk="login-password" type="password" class="form-control w-75" name="password" value="{{ old('password') }}" placeholder="Password" required="required">
+                        @if ($errors->has('password'))
+                            <span class="text-danger text-left">{{ $errors->first('password') }}</span>
+                        @endif
+                    </div>        
+                    <button dusk="login-button" class=" w-25 btn btn-primary m-3" type="submit">Se connecter</button>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
