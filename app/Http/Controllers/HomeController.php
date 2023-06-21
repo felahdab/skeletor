e347828856bad0008d6176826956c7024d468c97
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2023-06-21 08:03:35 +0000

    Remise en place du branding FFAST dans la page d'accueil du squelette

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,22 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Nwidart\Modules\Facades\Module;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+use Nwidart\Modules\Facades\Module;
 
 class HomeController extends Controller
 {
-    public function index() 
-    {
-        foreach(Module::allEnabled() as $module)
-        {
-            $module_home_route = $module->getLowerName() .".homeindex";
-            if (Route::has($module_home_route))
-            {
-                return redirect()->route($module_home_route);
+    public function index()
+    {   
+        if (Auth::check()) {
+            foreach (Module::allEnabled() as $module) {
+                $module_home_route = $module->getLowerName() . ".homeindex";
+                if (Route::has($module_home_route)) {
+                    return redirect()->route($module_home_route);
+                }
             }
-                
         }
         return view('home.index');
     }
