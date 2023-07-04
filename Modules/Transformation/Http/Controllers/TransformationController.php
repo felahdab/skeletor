01cb4a8c7144386ad5f3e20d4e86fff9e29ae9d7
sandrine <sandrine.zambelli@intradef.gouv.fr>
2023-07-04 06:41:40 +0000

    WIP remise en forme de la fiche bilan

--- Modules/Transformation/Http/Controllers/TransformationController.php
+++ Modules/Transformation/Http/Controllers/TransformationController.php
@@ -122,17 +122,17 @@ class TransformationController extends Controller
         foreach ($user->stages()->get() as $stage)
             array_push($liststage, $stage);
 
-        $nbcomp = count($listcomp);
-        $nbstage = count($liststage);
+        // $nbcomp = count($listcomp);
+        // $nbstage = count($liststage);
 
-        if ($nbcomp == $nbstage);
-        elseif ($nbcomp > $nbstage) {
-            $complement = array_fill(0, $nbcomp - $nbstage, null);
-            $liststage = array_merge($liststage, $complement);
-        } elseif ($nbcomp < $nbstage) {
-            $complement = array_fill(0, $nbstage - $nbcomp, null);
-            $listcomp = array_merge($listcomp, $complement);
-        }
+        // if ($nbcomp == $nbstage);
+        // elseif ($nbcomp > $nbstage) {
+        //     $complement = array_fill(0, $nbcomp - $nbstage, null);
+        //     $liststage = array_merge($liststage, $complement);
+        // } elseif ($nbcomp < $nbstage) {
+        //     $complement = array_fill(0, $nbstage - $nbcomp, null);
+        //     $listcomp = array_merge($listcomp, $complement);
+        // }
 
         // $readwrite=true;
         return view('transformation::transformation.fichebilan', [
--- Modules/Transformation/Resources/views/livewire/ffast-fiche-bilan.blade.php
+++ Modules/Transformation/Resources/views/livewire/ffast-fiche-bilan.blade.php
@@ -1,68 +1,113 @@
 <div style='width:100%; display:""; '
     wire:init="triggerLoad">
 
-    <table class=' mb-2 ' id ='ficheDeSynthese '  style='width: 100%; height: 95%; '>
-        <thead style='border: 1px solid #C3C3C3; font-weight: bold; font-size: large;'>
-            <tr>
-                <td colspan='2'><h1>{{$user->display_name}}</h1></td>
-                <td></td>
-                <td rowspan='4 '>@if($readyToLoad)<img src="{{ $user->getAnnudefPictureUrl() }}" width="150px">@endif</td>
-            </tr>
 
-            <tr>
-                <td colspan='2 '>Sp&eacute;cialit&eacute; :</td>
-                <td>{{$user->displaySpecialite()}}</td>
-            </tr>
-            <tr>
-                <td colspan='2'>Date d'embarquement :</td>
-                <td>{{$user->date_embarq}}</td>
-            </tr>
-            <tr>
-                <td colspan='2'>Taux de transformation :</td>
-                <td>{{round($user->taux_de_transformation, 2)}}%</td>
-            </tr>
-            @if ($user->displayDestination())
-            <tr>
-                <td colspan='2'>Unité destination :</td>
-                <td>{{$user->displayDestination()}}</td>
-                <td></td>
-                <td></td>
-            </tr>
-            @endif
-            <tr>
-                <td colspan='2'>Secteur :</td>
-                <td>{{$user->displaySecteur()}}</td>
-                <td></td>
-                <td></td>
-            </tr>
-            @if($readyToLoad)
-                @if ($user->getTransformationManager()->fonctionAQuai() != null)
-                    @foreach($user->getTransformationManager()->fonctionAQuai() as $fonctionAquai)
-                        <tr>
-                            <td colspan='2'>Fonction de service à quai :</td>
-                            <td>{{$fonctionAquai->fonction_libcourt}}</td>
-                            <td>{{ $fonctionAquai->pivot->date_lache ? 'LACHE' : 'NON LACHE'}}</td>
-                        </tr>
-                    @endforeach
-                @endif
-                @if ($user->getTransformationManager()->fonctionAMer() != null)
-                    @foreach($user->getTransformationManager()->fonctionAMer() as $fonctionAmer)
-                        <tr>
-                            <td colspan='2'>Fonction de service à la mer :</td>
-                            <td>{{$fonctionAmer->fonction_libcourt}}</td>
-                            <td>{{ $fonctionAmer->pivot->date_lache ? 'LACHE' : 'NON LACHE'}}</td>
-                        </tr>
-                    @endforeach
-                @endif
-                <tr>
-                    <td colspan='4'>&nbsp;</td>
-                </tr>
+    <div class="container">
+        <div class="row card">
+            <div class="col-12 card-header text-center"><h1>{{$user->display_name}}</h1></div>
+            <div class="container">
+                <div class="row justify-content-between mt-2 mb-2">
+                    <div class="col-2">
+                        @if($readyToLoad)<img src="{{ $user->getAnnudefPictureUrl() }}" width="150px">@endif
+                    </div>
+                    <div class="col-4">
+                        <div class="text-center mb-3 mt-3"><h1>{{round($user->taux_de_transformation, 2)}}%</h1></div>
+                        <div class="card "><div class=" m-2">
+                            <div><b>Sp&eacute;cialit&eacute; : </b>{{$user->displaySpecialite()}}</div>
+                            <div><b>Secteur : </b>{{$user->displaySecteur()}}</div>
+                            <div><b>Date d'embarquement : </b>{{$user->date_embarq}}</div>
+                            <div><b>Unité destination : </b>{{$user->displayDestination()}}</div>     
+                        </div></div>           
+                    </div>
+                    <div class="col-6 ">
+                        @if($readyToLoad)
+                        <div class="row card mb-2">
+                            @if ($user->getTransformationManager()->fonctionAQuai() != null)
+                                <div class="card-header"><b>Fonction(s) de service à quai</b></div>
+                                <div class="container ">
+                                    @foreach($user->getTransformationManager()->fonctionAQuai() as $fonctionAquai)
+                                    <div class="row">
+                                        <div class="col">{{$fonctionAquai->fonction_libcourt}}</div>
+                                        <div class="col">{{ $fonctionAquai->pivot->date_lache ? 'LACHE' : 'NON LACHE'}}</div>
+                                    </div>
+                                    @endforeach
+                                </div>
+                            @endif
+                        </div>
+                        <div class="row card mb-2">
+                            @if ($user->getTransformationManager()->fonctionAMer() != null)
+                                <div class="card-header "><b>Fonction(s) de service à la mer</b></div>
+                                <div class="container ">
+                                    @foreach($user->getTransformationManager()->fonctionAMer() as $fonctionAmer)
+                                    <div class="row">
+                                        <div class="col">{{$fonctionAmer->fonction_libcourt}}</div>
+                                        <div class="col">{{ $fonctionAmer->pivot->date_lache ? 'LACHE' : 'NON LACHE'}}</div>
+                                    </div>
+                                    @endforeach
+                                </div>
+                            @endif
+                        </div>                    
+                        <div class="row card mb-2">
+                            @if ($user->getTransformationManager()->fonctionsMetier() != null)
+                                <div class="card-header "><b>Fonction(s) métier</b></div>
+                                <div class="container ">
+                                    @foreach($user->getTransformationManager()->fonctionsMetier() as $fonctionmetier)
+                                    <div class="row">
+                                        <div class="col">{{$fonctionmetier->fonction_libcourt}}</div>
+                                        <div class="col">{{ $fonctionmetier->pivot->date_lache ? 'LACHE' : 'NON LACHE'}}</div>
+                                    </div>
+                                    @endforeach
+                                </div>
+                            @endif
+                        </div>                    
+                        @endif                                
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex flex-row justify-content-center">
+            <div class="p-2">
+                <div class="row card mb-2">
+                    @if (isset($listcomp))
+                    <div class="card-header "><b>COMPAGNONNAGES</b></div>
+                    <div class="container ">
+                        @foreach($listcomp as $comp)
+                            <div class="row">
+                                <div class="col">{{$comp->comp_libcourt}}</div>
+                                @php 
+                                    $pourcentage = $user->getTransformationManager()->taux_de_transformation(null, $comp, null, null);
+                                @endphp
+                                <x-ffast-progression-div :pourcentage="$pourcentage" height="whatever" class="col"/>
+                            </div>
+                            @endforeach
+                    </div>
+                    @endif
+                </div> 
+            </div>
+            <div class="p-2">STAGES</div>
+            
+
+        </div>
+
+        
+        
+        
+        
+        
+        
+    </div>
+
+
+    
+
+    <table class=' mb-2 ' id ='ficheDeSynthese '  style='width: 100%; height: 95%; '>
+  
                 <tr>
                     <!-- Entete fiche synthese -->
-                    <th colspan='2' class='text-center'>COMPAGNONNAGES</th>
-                    <th colspan='2' class='text-center'>STAGES</th>
+                    <th colspan='2' class='text-center'></th>
+                    <th colspan='2' class='text-center'></th>
                 </tr>
-            @endif
+
         </thead>
         @if($readyToLoad)
             <tbody style='border: 1px solid #C3C3C3;'>
