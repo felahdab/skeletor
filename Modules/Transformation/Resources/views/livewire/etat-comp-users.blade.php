449e24237308dc20e18ef3fde13e6b12a0a4fcf3
matthieu <matthieu.salaun@intradef.gouv.fr>
2023-11-15 10:42:17 +0000

    ihm

--- Modules/Transformation/Resources/views/livewire/etat-comp-users.blade.php
+++ Modules/Transformation/Resources/views/livewire/etat-comp-users.blade.php
@@ -33,36 +33,60 @@
         }
 
     }">
-        <div class="sticky-top" style="top:5rem;  background: white; width:100%; ">
-            <button type="submit" 
-            form="ssobjsusers"
-            class="btn btn-primary sticky-top" 
-            style="left:0;"
-            name="validation"
-            x-on:click.prevent="active = true ;
-                                validModal = new bootstrap.Modal(document.getElementById('divvalid'), []);
-                                validModal.show();
-                                buttonid = 'validation' ;"
-            x-on:uservalidated.window="if (active)
-                { 
-                    active = false; 
-                    $wire.ValideElementsDuParcoursParcomp( date_validation, commentaire, valideur, selected_parcomp );
-                }"
-                >Valider les éléments cochés
-            </button>
-        </div>
-        <div>Cliquez sur un sous objectif pour mettre la colonne en surbrillance</div>
-        <table class="table table-bordered table-striped table-hover table-sm" id="matable">
-            <thead class="sticky-top" style="top:7.5rem;">
-            <tr class="table-primary" >
-                <td colspan="3" style="position: sticky; left: 0px;z-index: 1;">
-                    <nav class="navbar bg-body-tertiary sticky-top">
-                        <div class="sticky-top" style="left:0px">
-                            <form style="left:0px;" role="search">
-                                <input type="search" class="form-control me-2" placeholder="Recherche nom" aria-label="Recherche" id="searchInput">
-                            </form>
+    <div>Cliquez sur un sous objectif pour mettre la colonne en surbrillance</div>
+    <div class="sticky-top" style="top:5rem;  background: white; width:100%; ">
+        <button type="submit" 
+        form="ssobjsusers"
+        class="btn btn-primary sticky-top" 
+        style="left:0;"
+        name="validation"
+        x-on:click.prevent="active = true ;
+                            validModal = new bootstrap.Modal(document.getElementById('divvalid'), []);
+                            validModal.show();
+                            buttonid = 'validation' ;"
+        x-on:uservalidated.window="if (active)
+            { 
+                active = false; 
+                $wire.ValideElementsDuParcoursParcomp( date_validation, commentaire, valideur, selected_parcomp );
+            }"
+            >Valider les éléments cochés
+        </button>
+
+        <div class="sticky-top" style="top:5.5rem;"> 
+            <div class="container p-0" style="float:left; position: sticky; left: 0px;z-index: 1;">
+                <div class="row">
+                    <div class="col-md-2">
+                        <nav class="navbar bg-body-tertiary sticky-top">
+                            <div class="sticky-top">
+                                <form role="search">
+                                    <input type="search" class="form-control me-2" placeholder="Recherche par nom" aria-label="Recherche" id="searchInput">
+                                </form>
+                            </div>
+                        </nav> 
+                    </div>
+                    <div class="col-md-3"> <!-- Ajustez la taille de la colonne ici -->
+                        <div class="input-group sticky-top mt-2">
+                            <input type="text" class="form-control" placeholder="Donner un nom au filtre" aria-label="nom du filtre" x-model="nomDuFiltre">
+                            <div class="input-group-append">
+                                <button  class="btn btn-primary sticky-top" title="enregistrer le filtre"  x-on:click="creerUnFiltre">
+                                    <img src="{!! asset("assets/images/floppy.svg") !!}" alt="">
+                                </button>
+                            </div>
                         </div>
-                    </nav> 
+                    </div>
+                    <div class="ml-auto">
+                        <span x-text ="erreur" class="text-danger"></span>
+                        <p class="text-success"> {{$success}} </p>
+                        <p class="text-danger">{{$erreur}}</p>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </div>
+        <table class="table table-bordered table-striped table-hover table-sm" id="matable">
+            <thead class="sticky-top" style="top:12.5rem;">
+            <tr class="table-primary" >
+                <td colspan="3">
                 </td>
                 @foreach($entete_taches as $entete_tache)
                     <td style="font-size:x-small;" colspan="{{$entete_tache['colspantach']}}" title="{{$entete_tache['libtach']}}">{{substr($entete_tache['libtach'], 0, 40)}}...</td>
@@ -70,13 +94,6 @@
             </tr>   
             <tr class="table-success">
                 <td colspan="3">
-                    <input type="text" class="form-control me-2" placeholder="Donner un nom au filtre" aria-label="nom du filtre" x-model="nomDuFiltre">
-                    <span x-text ="erreur" class="text-danger"></span>
-                    <p class="text-success"> {{$success}} </p>
-                    <p class="text-danger">{{$erreur}}</p>
-                    <button  class="btn btn-primary sticky-top" title="enregistrer le filtre"  x-on:click="creerUnFiltre">
-                        <img src="{!! asset("assets/images/floppy.svg") !!}" alt="">
-                    </button>
                     <div class="dropdown" class="sticky-top">
                         <button class="btn btn-primary dropdown-toggle" type="buttun" data-bs-toggle="dropdown" aria-expanded="false">
                             Filtre(s) enregistré(s)
@@ -91,7 +108,7 @@
                                 </li>
                             @endforeach
                         </ul>
-                    </div>    
+                    </div>  
                 </td>            
                 @foreach($entete_objectifs as $entete_objectif)
                     <td style="font-size:x-small;" colspan="{{$entete_objectif['colspanobj']}}" title="{{$entete_objectif['libobj']}}">{{substr($entete_objectif['libobj'], 0, 40)}}...</td>
