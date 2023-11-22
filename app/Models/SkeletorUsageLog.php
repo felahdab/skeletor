20c482c1fe6dce06b4fc4b695f326337ce8c670f
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2023-11-22 18:30:46 +0000

    Premiere version d'un outil analitique sur les usages de l'application. Enregistre les requetes ainsi que leurs principales metadonnees. Permet d'afficher les donners en ligne de commande avec artisan.

---
+++ app/Models/SkeletorUsageLog.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SkeletorUsageLog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'uri',
+        'route',
+        'session',
+        'source',
+        'user-agent',
+        'user-email',
+        'status',
+        'ip',
+        'method',
+        'counter'
+    ];
+}
