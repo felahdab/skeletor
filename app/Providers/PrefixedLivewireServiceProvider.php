1052bbd79bc6adf9e283093f353e0b30487961d4
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2023-11-23 20:12:07 +0000

    Further modifications to make the livewire prefix a configuration parameter.

--- app/Providers/PrefixedLivewireServiceProvider.php
+++ app/Providers/PrefixedLivewireServiceProvider.php
@@ -2,38 +2,23 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Route as RouteFacade;
+use Livewire\LivewireServiceProvider as BaseLivewireServiceProvider;
 
-use Illuminate\Support\ServiceProvider;
 
-use Livewire\Livewire;
-use Illuminate\Support\Facades\Route;
-
-class PrefixedLivewireServiceProvider extends ServiceProvider
+class PrefixedLivewireServiceProvider extends BaseLivewireServiceProvider
 {
-    public function boot()
+    protected function getMechanisms()
     {
-    }
-    public function register()
-    {
-        Livewire::setUpdateRoute(function ($handle) {
-            return Route::post('/ffast/livewire/update', $handle);
-        });
-
-        // Livewire::setUploadFileRoute(function ($handle) {
-        //     return Route::post('/ffast/livewire/upload-file', $handle);
-        // });
-
-        Livewire::setScriptRoute(function ($handle) {
-            return Route::get('/ffast/livewire/livewire.js', $handle);
-        });
-
-        return;
-
-        $prefix = config('livewire.route_prefix');
-
-        RouteFacade::prefix($prefix)->group(function () {
-            parent::registerRoutes();
-        });
+        return [
+            \Livewire\Mechanisms\PersistentMiddleware\PersistentMiddleware::class,
+            \Livewire\Mechanisms\HandleComponents\HandleComponents::class,
+            \App\Providers\LivewireMechanisms\HandleRequests::class,
+            \App\Providers\LivewireMechanisms\FrontendAssets::class,
+            \Livewire\Mechanisms\ExtendBlade\ExtendBlade::class,
+            \Livewire\Mechanisms\CompileLivewireTags::class,
+            \Livewire\Mechanisms\ComponentRegistry::class,
+            \Livewire\Mechanisms\RenderComponent::class,
+            \Livewire\Mechanisms\DataStore::class,
+        ];
     }
 }
