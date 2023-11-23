3f881592630cbc87766dad0878d89aea361d9966
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2023-11-23 21:14:38 +0000

    Re-enable injection of core and third party assets. Remove Alpine.js from main layout (since it is injected by livewire v3)

--- resources/views/layouts/app-master.blade.php
+++ resources/views/layouts/app-master.blade.php
@@ -67,7 +67,7 @@ Vous avez également la possibilité de télécharger directement une version po
     @livewireScripts
     <script src="{!! asset('assets/bootstrap/js/bootstrap.bundle.js') !!}"></script>
     <script src="{!! asset('/vendor/livewire-charts/app.js') !!}"></script>
-    <script src="{!! asset('assets/js/alpine.js') !!}"></script>
+    
     <script src="{!! asset('assets/js/apexcharts.js') !!}"></script>
     
     @yield("scripts")
