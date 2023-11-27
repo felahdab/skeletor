0dda5a2546a319a4e0eeb7a7dc0016e5b8917b63
matthieu <matthieu.salaun@intradef.gouv.fr>
2023-11-27 17:34:16 +0000

    sauv, création dos excel regroupant les fonctions, non finit

--- Modules/Transformation/Http/Livewire/FonctionList.php
+++ Modules/Transformation/Http/Livewire/FonctionList.php
@@ -4,11 +4,15 @@ namespace Modules\Transformation\Http\Livewire;
 
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Support\Facades\DB;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 use Modules\Transformation\Entities\Fonction;
 
 class FonctionList extends Component
 {
+    public $fileUrl  ='';
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
     
@@ -28,5 +32,28 @@ class FonctionList extends Component
                     ->OrderBy('fonction_libcourt')
                     ->paginate(10),
         ]);
+    }
+
+    public function enregistrerFonctions(){
+        ini_set('memory_limit', '512M');
+        set_time_limit(300);
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'Fonction Libcourt');
+        $sheet->setCellValue('B1', 'Fonction Liblong');
+        $fonctions = Fonction::limit(10)->get();
+        $row = 2;
+        foreach ($fonctions as $fonction) {
+            $sheet->setCellValue('A' . $row, $fonction->fonction_libcourt);
+            $sheet->setCellValue('B' . $row, $fonction->fonction_liblong);
+            $row++;
+        }
+        $fileName = 'TransformationFonctions_' . time() . '.xlsx';
+        $filePath = storage_path('app/public/' . $fileName);
+    
+        $writer = new Xlsx($spreadsheet);
+        $writer->save($filePath);
+    
+        $this->fileUrl = asset('storage/' . $fileName);
     }
 }
