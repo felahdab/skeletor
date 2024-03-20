eae55f128f3f2f7879b83d4b3bd0141fd7b375d5
sandrine <sandrine.zambelli@intradef.gouv.fr>
2024-03-20 07:17:47 +0000

    affichage page accueil par défaut

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,24 +15,47 @@ class HomeController extends Controller
     {
         $user = auth()->user();
 
+        // if ($user) {
+        //     $preferedroute = $user->settings()->get('prefered_page');
+
+        //     if ($preferedroute != null) {
+        //         return redirect()->route($preferedroute);
+        //     }
+
+        //     $paramaccueil = Paramaccueil::first();
+        //     if (!$paramaccueil) {
+        //         $paramaccueil = new Paramaccueil;
+        //         $paramaccueil->paramaccueil_image = '11.jpg';
+        //         $paramaccueil->paramaccueil_texte = 'le texte est modifiable';
+        //         $paramaccueil->save();
+        //     }
+        //     return view('home.index', ['paramaccueil' => $paramaccueil]);
+
+        // }
+
+        // return redirect()->route(config('skeletor.page_par_defaut'));
+
         if ($user) {
             $preferedroute = $user->settings()->get('prefered_page');
 
             if ($preferedroute != null) {
                 return redirect()->route($preferedroute);
             }
-
-            $paramaccueil = Paramaccueil::first();
-            if (!$paramaccueil) {
-                $paramaccueil = new Paramaccueil;
-                $paramaccueil->paramaccueil_image = '11.jpg';
-                $paramaccueil->paramaccueil_texte = 'le texte est modifiable';
-                $paramaccueil->save();
+            if (config('skeletor.page_par_defaut')!=''){
+                return redirect()->route(config('skeletor.page_par_defaut'));
             }
-            return view('home.index', ['paramaccueil' => $paramaccueil]);
-
+            else {
+                $paramaccueil = Paramaccueil::first();
+                if (!$paramaccueil) {
+                    $paramaccueil = new Paramaccueil;
+                    $paramaccueil->paramaccueil_image = '11.jpg';
+                    $paramaccueil->paramaccueil_texte = 'le texte est modifiable';
+                    $paramaccueil->save();
+                }
+                return view('home.index', ['paramaccueil' => $paramaccueil]);    
+            }        
         }
-
-        return redirect()->route(config('skeletor.page_par_defaut'));
+        return view('auth.login');
+        
     }
 }
