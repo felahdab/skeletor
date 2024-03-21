12b72e3db7cc934cb7385e9d89dc4f9fcafbc755
CV Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2024-03-21 08:32:32 +0000

    Ajout de fichiers ignorés mais necessaires

---
+++ Modules/Transformation/Views/Components/FfastFonctionText.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Modules\Transformation\Views\Components;
+
+use Illuminate\View\Component;
+
+class FfastFonctionText extends Component
+{
+    
+    public $text="";
+    public $pourcentage=0;
+    public $lache = false;
+    public $caption="";
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($text="", $pourcentage=0, $lache = false)
+    {
+        $this->text = $text;
+        $this->pourcentage = $pourcentage;
+        $this->lache = $lache;
+
+        if ($this->text == "")
+        {
+            $this->caption = $this->pourcentage . "%";
+        }
+        else
+        {
+            $this->caption = $this->text . " (" . $this->pourcentage . "%)";
+        }
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        if ($this->lache)
+            $color = 'green';
+        elseif ($this->pourcentage >= 70 )
+            $color = 'Goldenrod';
+        elseif ($this->pourcentage >= 35)
+            $color = 'orangered';
+        else
+            $color = 'red';
+        
+        return view('transformation::components.ffast-fonction-text', ['color' => $color,
+                                                     'caption' => $this->caption]);
+    }
+}
