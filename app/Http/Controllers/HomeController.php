ee770f35d75976148c642106ff44cc505471f569
CV Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2024-03-21 08:49:37 +0000

    wip

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,21 +21,18 @@ class HomeController extends Controller
             if ($preferedroute != null) {
                 return redirect()->route($preferedroute);
             }
-            if (config('skeletor.page_par_defaut')!=''){
+            if (config('skeletor.page_par_defaut') != '') {
                 return redirect()->route(config('skeletor.page_par_defaut'));
             }
-            else {
-                $paramaccueil = Paramaccueil::first();
-                if (!$paramaccueil) {
-                    $paramaccueil = new Paramaccueil;
-                    $paramaccueil->paramaccueil_image = '11.jpg';
-                    $paramaccueil->paramaccueil_texte = 'le texte est modifiable';
-                    $paramaccueil->save();
-                }
-                return view('home.index', ['paramaccueil' => $paramaccueil]);    
-            }        
         }
-        return view('auth.login');
         
+        $paramaccueil = Paramaccueil::first();
+        if (!$paramaccueil) {
+            $paramaccueil = new Paramaccueil;
+            $paramaccueil->paramaccueil_image = '11.jpg';
+            $paramaccueil->paramaccueil_texte = 'le texte est modifiable';
+            $paramaccueil->save();
+        }
+        return view('home.index', ['paramaccueil' => $paramaccueil]);
     }
 }
