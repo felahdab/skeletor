5f852f68bf2d9f665f74d6910978052b4731646d
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2024-07-03 18:50:47 +0200

    Ajustement de la configuration vite.

--- vite.config.js
+++ vite.config.js
@@ -2,39 +2,52 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { resolve } from 'path';
 import path from 'path';
+import collectModuleAssetsPaths from './vite-module-loader.js';
 
 // import react from '@vitejs/plugin-react';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    build : {
-        outDir: './public/assets/build/',
-        emptyOutDir: true,
-        manifest: true
-    },
-    resolve: {
-        alias: {
-            "@mingle": path.resolve("/app/vendor/ijpatricio/mingle/resources/js"),
+
+async function getConfig() {
+    const topPaths = [
+        'resources/js/app.js',
+        'resources/css/app.css',
+    ];
+
+    //const allPaths = await collectModuleAssetsPaths(topPaths, 'Modules');
+
+    console.log(topPaths);
+
+    return defineConfig({
+        build : {
+            outDir: './public/assets/build/',
+            emptyOutDir: true,
+            manifest: true,
         },
-    },
-    plugins: [
-        laravel({
-            input: [
-                'resources/js/app.js',
-                'resources/css/app.css',
-            ],
-            refresh: true,
-        }),
         
-        // react(),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
+        resolve: {
+            alias: {
+                "@mingle": path.resolve("/app/vendor/ijpatricio/mingle/resources/js"),
             },
-        }),
-    ],
-    host: "0.0.0.0"
-});
+        },
+        plugins: [
+            laravel({
+                input: topPaths,
+                refresh: true,
+            }),
+            
+            // react(),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+        host: "0.0.0.0"
+    });
+}
+
+export default getConfig();
