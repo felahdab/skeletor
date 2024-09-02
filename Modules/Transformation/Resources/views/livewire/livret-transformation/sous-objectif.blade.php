1cab4335634efda9818d4ab8cf84eec9864b6d65
sandrine <sandrine.zambelli@intradef.gouv.fr>
2024-09-02 09:28:33 +0000

    evolution affichage livret pour FCM

--- Modules/Transformation/Resources/views/livewire/livret-transformation/sous-objectif.blade.php
+++ Modules/Transformation/Resources/views/livewire/livret-transformation/sous-objectif.blade.php
@@ -4,7 +4,7 @@
     @endif
     {{$sous_objectif->ssobj_lib}}
 </td>
-<td>{{$sous_objectif->ssobj_duree}}</td>
+{{-- <td>{{$sous_objectif->ssobj_duree}}</td> --}}
 <td>
     @if ($mode == "validelacherdouble" || $mode == "consultation" || ($mode== 'proposition' && $user->getTransformationManager()->aValideLeSousObjectif($sous_objectif) )  )
     @else
@@ -31,8 +31,9 @@
         </span>
     @endif
     @if ($mode!='modificationmultiple'  && $user->getTransformationManager()->aValideLeSousObjectif($sous_objectif))
-        <button class='btn btn-success' type='button' disabled>
-        VALIDE {{ $user->getTransformationManager()->dateDeValidationDuSousObjectif($sous_objectif) }}
+        <button class='btn btn-success  btn-sm' type='button' disabled>
+        VALIDE <br>{{ $user->getTransformationManager()->dateDeValidationDuSousObjectif($sous_objectif) }}<br>
+        <span style="font-size:smaller">{{ $user->getTransformationManager()->valideurDeValidationDuSousObjectif($sous_objectif) }}</span>
         </button>
     @endif
     @if ($mode!='modificationmultiple'  && $user->getTransformationManager()->aProposeLeSousObjectif($sous_objectif))
@@ -41,12 +42,12 @@
         </button>
     @endif
 </td>
-@if ($mode!='modificationmultiple' && ( $user->getTransformationManager()->aValideLeSousObjectif($sous_objectif) || $user->getTransformationManager()->aProposeLeSousObjectif($sous_objectif) ) )
+{{-- @if ($mode!='modificationmultiple' && ( $user->getTransformationManager()->aValideLeSousObjectif($sous_objectif) || $user->getTransformationManager()->aProposeLeSousObjectif($sous_objectif) ) )
     <td>
         {{ $user->getTransformationManager()->valideurDeValidationDuSousObjectif($sous_objectif) }}
     </td>
 @else
     <td>&nbsp;</td>
-@endif
-<td>{{$sous_objectif->lieu->lieu_libcourt}}</td>
+@endif --}}
+{{-- <td>{{$sous_objectif->lieu->lieu_libcourt}}</td> --}}
 </tr>
