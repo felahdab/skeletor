748e599686dcd177ca688ed25ef1e17cb9898888
Florian El-Ahdab <florian.el-ahdab@adalfantln.marine.defensecdd.gouv.fr>
2024-10-30 12:25:46 +0100

    Modification valentin-morice pour inclure jsoneditor en local.

--- vendor/valentin-morice/filament-json-column/src/FilamentJsonColumnServiceProvider.php
+++ vendor/valentin-morice/filament-json-column/src/FilamentJsonColumnServiceProvider.php
@@ -28,10 +28,10 @@ class FilamentJsonColumnServiceProvider extends PackageServiceProvider
     public function packageBooted(): void
     {
         FilamentAsset::register([
-            Js::make('jsoneditor-js-cdn', 'https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/10.0.2/jsoneditor.min.js'),
+            Js::make('jsoneditor-js-cdn', __DIR__ . '/../resources/js/jsoneditor.min.js'),
             Js::make('app', __DIR__ . '/../resources/js/app.js'),
             Css::make('app', __DIR__ . '/../resources/css/app.css'),
-            Css::make('jsoneditor-css-cdn', 'https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/10.0.2/jsoneditor.min.css'),
+            Css::make('jsoneditor-css-cdn', __DIR__ . '/../resources/css/jsoneditor.min.css'),
         ]);
     }
 }
