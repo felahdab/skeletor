dd4505a0fe9b8a5f6c8e3e3a79886101594c93d6
Florian El-Ahdab <florian.el-ahdab@adalfantln.marine.defensecdd.gouv.fr>
2024-11-25 21:41:38 +0100

    Création de la commande de génération de la doc des api

---
+++ app/Console/Commands/GenerateApiDocsJsonCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+
+use Illuminate\Support\Facades\Artisan;
+
+
+class GenerateApiDocsJsonCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'skeletor:generate-api-docs';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Cette commande génère le fichier api-docs.json nécessaire pour le fonctionnement de SwaggerUI.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        /**
+         *  Cette commande repose sur scramble:export qui réalise le plus gros du travail.
+         *  Puis, cette commande récupère le fichier api-docs.json et le nettoie pour que le rendu avec swagger UI soit plus
+         *  propre.
+         */
+        Artisan::call('scramble:export');
+
+        $apidocspath = config('scramble.export_path');
+        $api_doc = json_decode(file_get_contents($apidocspath), true);
+
+        foreach ($api_doc["paths"] as $path => $path_description)
+        {
+            foreach ($path_description as $method => $method_description){
+                $tags = Arr::get($api_doc, "paths." . $path. "." . $method . ".tags");
+                Arr::set($api_doc, "paths." . $path. "." . $method . ".tags", [ $tags[0] ]);
+            }
+        }
+        
+        file_put_contents($apidocspath, json_encode($api_doc));
+    }
+}
