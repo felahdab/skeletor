bc5fc0a11d84aae5da7fdb5b41586f35d1dff77d
CV Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2026-01-30 07:21:54 +0000

    Implements artifact #673621: permet au bouton d'ouverture du modal de signalement de bug ou de suggestion de fonctionner également lorsque le panneau filament actif a été configuré pour utiliser la topNavigation.

--- resources/views/layouts/partials/trigger-report-bug-or-suggestion.blade.php
+++ resources/views/layouts/partials/trigger-report-bug-or-suggestion.blade.php
@@ -1,3 +1,3 @@
-<x-filament::button x-on:click="$dispatch('open-modal', { id: 'report-bug-or-suggestion' });" icon='heroicon-o-light-bulb'>
+<x-filament::button x-data={} x-on:click="$dispatch('open-modal', { id: 'report-bug-or-suggestion' });" icon='heroicon-o-light-bulb'>
     Bug ? Suggestion ?
 </x-filament::button>
