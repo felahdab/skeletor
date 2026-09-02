53a29bb2a0f53ab415076dfaa2aeadfd292f43fe
CV Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2026-09-02 06:17:48 +0000

    Ajustement SkeletorRouteServiceProvider pour tenir compte du prefixe aleatoire des routes Livewire

--- app/Providers/SkeletorRouteServiceProvider.php
+++ app/Providers/SkeletorRouteServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
+use Livewire\Mechanisms\HandleRequests\EndpointResolver;
 
 class SkeletorRouteServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,8 @@ class SkeletorRouteServiceProvider extends ServiceProvider
     {
         $prefix = config('skeletor.prefixe_instance');
 
+        $livewire_prefix = Str::after(EndpointResolver::prefix(), '/');
+
         foreach (Route::getRoutes() as $route) {
             if (Str::is('filament/exports/{export}/download', $route->uri())) {
                 $route->setUri($prefix.'/'.$route->uri());
@@ -27,34 +30,34 @@ class SkeletorRouteServiceProvider extends ServiceProvider
             if (Str::is('filament/imports/{import}/failed-rows/download', $route->uri())) {
                 $route->setUri($prefix.'/'.$route->uri());
             }
-            if (Str::is('livewire-c76b996c/livewire.js', $route->uri())) {
+            if (Str::is("{$livewire_prefix}/livewire.js", $route->uri())) {
                 $route->setUri($prefix.'/'.$route->uri());
             }
-            if (Str::is('livewire-c76b996c/livewire.min.js', $route->uri())) {
+            if (Str::is("{$livewire_prefix}/livewire.min.js", $route->uri())) {
                 $route->setUri($prefix.'/'.$route->uri());
             }
-            if (Str::is('livewire-c76b996c/livewire.min.js.map', $route->uri())) {
+            if (Str::is("{$livewire_prefix}/livewire.min.js.map", $route->uri())) {
                 $route->setUri($prefix.'/'.$route->uri());
             }
-            if (Str::is('livewire-c76b996c/preview-file/{filename}', $route->uri())) {
+            if (Str::is("{$livewire_prefix}/preview-file/{filename}", $route->uri())) {
                 $route->setUri($prefix.'/'.$route->uri());
             }
-            if (Str::is('livewire-c76b996c/update', $route->uri())) {
+            if (Str::is("{$livewire_prefix}/update", $route->uri())) {
                 $route->setUri($prefix.'/'.$route->uri());
             }
-            if (Str::is('livewire-c76b996c/upload-file', $route->uri())) {
+            if (Str::is("{$livewire_prefix}/upload-file", $route->uri())) {
                 $route->setUri($prefix.'/'.$route->uri());
             }
-            if (Str::is('livewire-c76b996c/css/{component}.css', $route->uri())) {
+            if (Str::is("{$livewire_prefix}/css/{component}.css", $route->uri())) {
                 $route->setUri($prefix.'/'.$route->uri());
             }
-            if (Str::is('livewire-c76b996c/css/{component}.global.css', $route->uri())) {
+            if (Str::is("{$livewire_prefix}/css/{component}.global.css", $route->uri())) {
                 $route->setUri($prefix.'/'.$route->uri());
             }
-            if (Str::is('livewire-c76b996c/js/{component}.js', $route->uri())) {
+            if (Str::is("{$livewire_prefix}/js/{component}.js", $route->uri())) {
                 $route->setUri($prefix.'/'.$route->uri());
             }
-            if (Str::is('livewire-c76b996c/livewire.csp.min.js.map', $route->uri())) {
+            if (Str::is("{$livewire_prefix}/livewire.csp.min.js.map", $route->uri())) {
                 $route->setUri($prefix.'/'.$route->uri());
             }
             if (Str::is('tenancy/assets/{path?}', $route->uri())) {
